675cb3967978a3e786a4dbc5479d60ccf489b9b7
olwend <olwend@gmail.com>
2017-03-09 23:03:07 +0000

    Statement print methods tests green

--- lib/statement.rb
+++ lib/statement.rb
@@ -12,14 +12,18 @@ class Statement
   end
 
   def print_statement
+  prep_body
+  @header + @body
   end
 
   def credit_or_debit(transaction)
     transaction.amount > 0 ? credit = transaction.amount : debit = transaction.amount.abs
   end
 
-  # def prep_body
-  #   @transactions.each do |trans|
-  #   trans.amount > 0 ? credit = trans.amount : debit = trans.amount.abs
-  # end
+  def prep_body
+    @transactions.each do |trans|
+    trans.amount > 0 ? credit = trans.amount : debit = trans.amount.abs
+    @body += "||#{trans.date}   ||" '%10s' % "#{credit}||" + '%11s' % " #{debit}||" + '%9s' % "#{trans.balance}\n"
+    end
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,14 +4,16 @@ describe Statement do
   subject (:statement) { described_class.new }
 
   before(:each) do
-  transaction1 = Transaction.new(200,600)
-  subject.build_transactions(transaction1)
-  transaction2 = Transaction.new(-300,0)
-  subject.build_transactions(transaction2)
+  transaction3 = Transaction.new(1000,1000)
+  subject.build_transactions(transaction3)
+  transaction4 = Transaction.new(2000,3000)
+  subject.build_transactions(transaction4)
+  transaction5 = Transaction.new(-500,2500)
+  subject.build_transactions(transaction5)
   end
 
   it "holds transactions details in a hash" do
-    expect(subject.transactions.size).to eq (2)
+    expect(subject.transactions.size).to eq (3)
   end
 
   it "has a header" do
@@ -19,12 +21,15 @@ describe Statement do
   end
 
   it "transforms transaction into credit or debit" do
-    transaction1 = Transaction.new(200,600)
-    expect(subject.credit_or_debit(transaction1)).to eq(200)
-    transaction1 = Transaction.new(-300,600)
-    expect(subject.credit_or_debit(transaction1)).to eq(300)
+    transaction1 = Transaction.new(1000,1000)
+    expect(subject.credit_or_debit(transaction1)).to eq(1000)
+    transaction2 = Transaction.new(2000,3000)
+    expect(subject.credit_or_debit(transaction2)).to eq(2000)
+    transaction3 = Transaction.new(-500,2500)
+    expect(subject.credit_or_debit(transaction3)).to eq(500)
   end
 
-  it "lists transactions most recent first" do
+  it "lists transactions most recent first - visual check" do
+    p subject.print_statement
   end
 end
