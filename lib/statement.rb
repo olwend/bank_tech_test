2844f6e41f01fd9a0591d335765ed203351b0e7e
olwend <olwend@gmail.com>
2017-03-09 22:04:46 +0000

    Statement structure and methods

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,10 @@
 class Statement
-  attr_reader :transactions
+  attr_reader :transactions, :body, :header
 
   def initialize
     @transactions = []
+    @body = ''
+    @header = "date       || credit || debit   || balance"
   end
 
   def build_transactions(transaction)
@@ -11,4 +13,13 @@ class Statement
 
   def print_statement
   end
+
+  def credit_or_debit(transaction)
+    transaction.amount > 0 ? credit = transaction.amount : debit = transaction.amount.abs
+  end
+
+  # def prep_body
+  #   @transactions.each do |trans|
+  #   trans.amount > 0 ? credit = trans.amount : debit = trans.amount.abs
+  # end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,10 +1,28 @@
 require_relative '../lib/statement'
 describe Statement do
 
-  it "gets a transaction from transactions hash" do
+  subject (:statement) { described_class.new }
+
+  before(:each) do
+  transaction1 = Transaction.new(200,600)
+  subject.build_transactions(transaction1)
+  transaction2 = Transaction.new(-300,0)
+  subject.build_transactions(transaction2)
+  end
+
+  it "holds transactions details in a hash" do
+    expect(subject.transactions.size).to eq (2)
   end
 
   it "has a header" do
+    expect(subject.header).to be
+  end
+
+  it "transforms transaction into credit or debit" do
+    transaction1 = Transaction.new(200,600)
+    expect(subject.credit_or_debit(transaction1)).to eq(200)
+    transaction1 = Transaction.new(-300,600)
+    expect(subject.credit_or_debit(transaction1)).to eq(300)
   end
 
   it "lists transactions most recent first" do
